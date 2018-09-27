2182f12e1259f19cacabdfc9867740d09e95b337
Phan Van Luan <phanvanluan611996@gmail.com>
2018-09-27 09:44:52 +0700

    remove image seeding task

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -26,7 +26,6 @@ class TasksTableSeeder extends Seeder
         $task->content          = "Nội dung công việc 2";
         $task->image            = "";
         $task->due_date         = "2018-09-16";
-        $task->image            = "";
         $task->category_id      = 1;
         $task->save();
 
